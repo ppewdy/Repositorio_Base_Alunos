c1e1037eba1def607f6ec4019a763dfd901513c4
pewdro <mr.peterfarinha@gmail.com>
2025-09-11 13:30:13 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/ex001.py
@@ -0,0 +1,12 @@
+num1 = int(input("Digite o primeiro numero"))
+num2 = int(input("Digite o primeiro numero"))
+
+adicao = num1 + num2
+sub =  num1 - num2
+mult = num1 * num2
+
+
+
+print ("adição = ", adicao)
+print ("subtração = ", sub)
+print ("multiplicação = ", mult)
